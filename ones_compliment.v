b54a8920f00074b4018e23bb382b32b1c64c30de
Caleb-ops-sudo <calebcook766@u.boisestate.edu>
2025-02-23 16:27:12 -0700

    Update ones_compliment.v

--- ones_compliment.v
+++ ones_compliment.v
@@ -1,83 +1,89 @@
 module onescompliment(
     input [3:0] A, // Four bits of A: 3, 2, 1, 0
     input [3:0] B, // Same for B
-    output [3:0] Y // and for Y
+    output [3:0] sum // and for Y
 );
     
-    wire around;
-    wire [3:0] carry ;
-    wire [3:0] second_carry;
-    wire [3:0] realcarries ;
+    
+    wire carry0;
+    wire carry1;
+    wire carry2;
+    wire carry3;
     wire [3:0] intersum;
     
     //firstaddittion
     
-    full_adder lsb_1(
+    fulladder A0(
     .A(A[0]),
     .B(B[0]),
     .Y(intersum[0]),
-    .Cin(1'b0),
-    .Cout(carry[0])
+    .C(1'b0),
+    .Cout(carry0)
     );
     
-    full_adder msb_1(
+    fulladder A1(
     .A(A[1]),
     .B(B[1]),
     .Y(intersum[1]),
-    .Cin(carry[0]),
-    .Cout(carry[1])
+    .C(carry0),
+    .Cout(carry1)
     );
     
-    full_adder lsb_2(
+    fulladder A2(
     .A(A[2]),
     .B(B[2]),
     .Y(intersum[2]),
-    .Cin(carry[1]),
-    .Cout(carry[2])
+    .C(carry1),
+    .Cout(carry2)
     );
     
-    full_adder msb_2(
+    fulladder A3(
     .A(A[3]),
     .B(B[3]),
     .Y(intersum[3]),
-    .Cin(carry[2]),
-    .Cout(around)
+    .C(carry2),
+    .Cout(carry3)
     );
     
     //second addition 
     
-    full_adder lsb_a(
+    wire [3:0] carrysum;
+    wire carry000;
+    wire carry001;
+    wire carry010;
+    wire carry011;
+    
+    fulladder A4(
     .A(intersum[0]),
-    .B(1'b0),
-    .Y(Y[0]),
-    .Cin(around),
-    .Cout(second_carry[0])
+    .B(carry03),
+    .Y(sum[0]),
+    .C(1'b0),
+    .Cout(carry000)
     );
     
-    full_adder msb_b(
+    fulladder A5(
     .A(intersum[1]),
     .B(1'b0),
-    .Y(Y[1]),
-    .Cin(second_carry[0]),
-    .Cout(second_carry[1])
+    .Y(sum[1]),
+    .C(carry000),
+    .Cout(carry001)
     );
     
-     full_adder lsb_c(
+    fulladder A6(
     .A(intersum[2]),
     .B(1'b0),
-    .Y(Y[2]),
-    .Cin(second_carry[1]),
-    .Cout(second_carry[2])
+    .Y(sum[2]),
+    .C(carry001),
+    .Cout(carry010)
     );
     
-    full_adder lsb_d(
+    fulladder A7(
     .A(intersum[3]),
     .B(1'b0),
-    .Y(Y[3]),
-    .Cin(second_carry[2])
+    .Y(sum[3]),
+    .C(carry010),
+    .Cout(carry011)
     );
-
-
 
 
 endmodule
